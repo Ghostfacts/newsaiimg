0ccddb6fa1d9e828157f4b3e7566940e765f799e
forestnd <nig@ghostfacts.co.uk>
2024-12-08 21:39:05 +0000

    updating s3 bucket blocking public access

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,3 +5,9 @@ resource "aws_s3_bucket" "aiminnews" {
     Environment = "Dev"
   }
 }
+
+ resource "aws_s3_bucket_public_access_block" "access_good_1" {
+  bucket = aws_s3_bucket.aiminnews.id
+  block_public_acls   = true
+  block_public_policy = true
+ }
