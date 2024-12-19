d5ca588bef9040d8a56446cac8bc396ccf0fd263
forestnd <nig@ghostfacts.co.uk>
2024-12-19 09:34:01 +0000

    updating terraform code on roles

---
+++ terraform/modules/lambda_function/role.tf
@@ -0,0 +1,15 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name               = ${local.role_name}
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
