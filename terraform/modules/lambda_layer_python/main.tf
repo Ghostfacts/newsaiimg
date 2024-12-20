3b18132673ccd3cdd657c89b7814a4ebe3e9350f
forestnd <nig@ghostfacts.co.uk>
2024-12-20 00:03:51 +0000

    updated layer code to not all ways run

--- terraform/modules/lambda_layer_python/main.tf
+++ terraform/modules/lambda_layer_python/main.tf
@@ -3,7 +3,8 @@ resource "null_resource" "make_tmp_folder" {
     command = "mkdir /tmp/${var.layer_name}/"
   }
   triggers = {
-    always_run = "${timestamp()}" #uncomment to tigger all the time    
+    # always_run = "${timestamp()}" #uncomment to tigger all the time
+    module_change = "${var.modules}"
   }
 }
 
@@ -14,7 +15,8 @@ resource "null_resource" "pip_install" {
     command = "${var.runtime} -m pip install ${each.value} --no-cache-dir --upgrade --isolated --target /tmp/${var.layer_name}/python/lib/${var.runtime}/site-packages/"
   }
   triggers = {
-    always_run = "${timestamp()}" #uncomment to tigger all the time
+    # always_run = "${timestamp()}" #uncomment to tigger all the time
+    module_change = "${var.modules}"
   }
   depends_on =[
     null_resource.make_tmp_folder
