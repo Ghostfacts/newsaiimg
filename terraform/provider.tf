7b1f6de44c67b8704cffbc62670737f18f60a648
forestnd <nig@ghostfacts.co.uk>
2024-12-21 22:02:10 +0000

    removing old resources

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = "eu-west-1"
   assume_role {
     role_arn     = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_role_name}"
     session_name = "terraform"
--- terraform/step_function.tf
+++ terraform/step_function.tf
@@ -1,46 +1,43 @@
-#Roles
-resource "aws_iam_role" "step_function_role" {
-  name = "step_function_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "states.${data.aws_region.current.name}.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
+# #Roles
+# resource "aws_iam_role" "step_function_role" {
+#   name = "step_function_role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "states.${data.aws_region.current.name}.amazonaws.com"
+#         }
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy" "step_function_policy" {
-  name   = "step_function_policy"
-  role   = aws_iam_role.step_function_role.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "lambda:InvokeFunction"
-        ]
-        Effect   = "Allow"
-        Resource = "${module.news_api_function.function.arn}*"
-      }
-    ]
-  })
-}
+# resource "aws_iam_role_policy" "step_function_policy" {
+#   name   = "step_function_policy"
+#   role   = aws_iam_role.step_function_role.id
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = [
+#           "lambda:InvokeFunction"
+#         ]
+#         Effect   = "Allow"
+#         Resource = "${module.news_api_function.function.arn}*"
+#       }
+#     ]
+#   })
+# }
 
-#function it self
-resource "aws_sfn_state_machine" "example" {
-  # checkov:skip=CKV_AWS_285
-  name     = "newsaiimg-${local.environment_map[var.environment]}-step-function"
-  role_arn = aws_iam_role.step_function_role.arn
-  definition = templatefile("${path.module}/files/stepfunction_plan.json.tpl", {
-    newapi_lmb_function = "${module.news_api_function.function.arn}"
-  })
-
-
-
-}
+# #function it self
+# resource "aws_sfn_state_machine" "example" {
+#   # checkov:skip=CKV_AWS_285
+#   name     = "newsaiimg-${local.environment_map[var.environment]}-step-function"
+#   role_arn = aws_iam_role.step_function_role.arn
+#   definition = templatefile("${path.module}/files/stepfunction_plan.json.tpl", {
+#     newapi_lmb_function = "${module.news_api_function.function.arn}"
+#   })
+# }
