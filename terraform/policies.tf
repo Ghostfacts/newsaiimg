b78855befd06dea84c7816c45382abb3779db05c
forestnd <nig@ghostfacts.co.uk>
2024-12-21 22:07:39 +0000

    switching over to london

--- terraform/policies.tf
+++ terraform/policies.tf
@@ -1,10 +1,10 @@
-# data "aws_iam_policy_document" "lambda_policy" {
-#   statement {
-#     effect = "Allow"
-#     actions = [
-#       	"secretsmanager:GetSecretValue",
-# 				"secretsmanager:ListSecretVersionIds"
-#     ]
-#     resources = [aws_secretsmanager_secret.newsapi.arn]
-#   }
-# }
+data "aws_iam_policy_document" "lambda_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      	"secretsmanager:GetSecretValue",
+				"secretsmanager:ListSecretVersionIds"
+    ]
+    resources = [aws_secretsmanager_secret.newsapi.arn]
+  }
+}
