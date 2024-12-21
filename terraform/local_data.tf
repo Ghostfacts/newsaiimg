00087024257fc0711656e2117495ba9a9e596ef7
forestnd <nig@ghostfacts.co.uk>
2024-12-21 22:31:51 +0000

    Updting code to include region in name

--- terraform/local_data.tf
+++ terraform/local_data.tf
@@ -4,10 +4,18 @@ locals {
      "staging"     = "stg"
      "production"  = "prod"
     }
-    resource_naming_prefex="newsaiimg-${local.environment_map[var.environment]}-"
+    region_map ={
+        "eu-west-1"    = "dub",
+        "eu-west-2"    = "lon",
+        "eu-west-3"    = "par",
+        "eu-central-1" = "fra",
+        "us-east-1"    = "nyc"
+    }
+    resource_naming_prefex="newsaiimg-${local.environment_map[var.environment]}-${local.region_map[data.aws_region.current.name]}-"
     tags={
         project = "newsaiimg"
         environment = var.environment
+        region = local.region_map[data.aws_region.current.name]
     }
 }
 
