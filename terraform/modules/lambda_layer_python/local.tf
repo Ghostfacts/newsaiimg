efa3ee888e4a2f5f75062ef70e5d065c971c93b3
forestnd <nig@ghostfacts.co.uk>
2024-12-22 00:37:13 +0000

    adding prcommit as well as change to the lambda layer module

--- terraform/modules/lambda_layer_python/local.tf
+++ terraform/modules/lambda_layer_python/local.tf
@@ -1,5 +1,5 @@
 locals {
-  modules_hash = "${md5(join("", var.modules))}"
-  runtime_hash = "${md5(var.runtime)}"
+  modules_hash = "${md5(join("", var.modules))}",
+  runtime_hash = "${md5(var.runtime)}",
   combined_hash = "${md5(modules_hash ~ runtime_hash)}"
 }
