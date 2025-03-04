c5d26017060fd2051e2931172a5473cfe88f82b0
forstops <97102660+forstops@users.noreply.github.com>
2025-03-04 16:25:41 +0000

    fixing terraform module issue

--- terraform/modules/lambda_layer_python/main.tf
+++ terraform/modules/lambda_layer_python/main.tf
@@ -2,11 +2,6 @@ resource "null_resource" "make_tmp_folder" {
   provisioner "local-exec" {
     command = "mkdir /tmp/${var.layer_name}/"
   }
-  triggers = {
-    # always_run   = "${timestamp()}",
-    runtime      = local.runtime_hash,
-    modules_hash = md5(join("", var.modules))
-  }
 }
 
 resource "null_resource" "pip_install" {
@@ -14,11 +9,6 @@ resource "null_resource" "pip_install" {
   for_each = toset(var.modules)
   provisioner "local-exec" {
     command = "${var.runtime} -m pip install ${each.value} --no-cache-dir --upgrade --isolated --target /tmp/${var.layer_name}/python/lib/${var.runtime}/site-packages/"
-  }
-  triggers = {
-    # always_run   = "${timestamp()}",
-    runtime      = local.runtime_hash,
-    modules_hash = md5(join("", var.modules))
   }
   depends_on = [
     null_resource.make_tmp_folder
@@ -35,7 +25,22 @@ data "archive_file" "layerzip" {
   ]
 }
 
+
+resource "null_resource" "layer_check" {
+  triggers = {
+    # always_run   = "${timestamp()}",
+    runtime      = local.runtime_hash,
+    modules_hash = md5(join("", var.modules))
+  }
+}
+
+locals {
+  create_layer = length(null_resource.layer_check.triggers) > 0 ? true : false
+}
+
+
 resource "aws_lambda_layer_version" "layer" {
+  count               = local.create_layer ? 1 : 0
   filename            = data.archive_file.layerzip.output_path
   layer_name          = var.layer_name
   source_code_hash    = data.archive_file.layerzip.output_base64sha256
