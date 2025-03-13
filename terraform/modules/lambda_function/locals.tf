908d068537f97e023f36fb6a7d1d85eb610a3717
forestnd <nig@ghostfacts.co.uk>
2025-03-13 21:20:41 +0000

    Update code for function trigger

--- terraform/modules/lambda_function/locals.tf
+++ terraform/modules/lambda_function/locals.tf
@@ -2,5 +2,6 @@ locals {
   tags          = merge(var.tags)
   lambda_name   = "${var.nameprefex}-lambda-function"
   role_name     = "${var.nameprefex}-lambda_iam_role"
-  function_hash = md5(join("", var.source_path))
+  files         = [for file in fileset(var.source_path, "**/*") : file]
+  function_hash = md5(join("", [for file in local.files : filemd5(file)]))
 }
