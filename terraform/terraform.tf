a6d125894d0e704ad6df0d1ef1f5b26df5652d1c
Nigel <forestnd@users.noreply.github.com>
2025-03-13 21:09:10 +0000

    Bedrock (#32)
    * Adding correct role permisions
    
    * Adding correct role permisions
    
    * Adding correct role permisions
    
    * Adding correct role permisions
    
    * Adding correct role permisions
    
    * Adding correct role permisions
    
    * updating the step function
    
    * adding the event bridge rule
    
    * adding prcommit as well as change to the lambda layer module
    
    * adding prcommit as well as change to the lambda layer module
    
    * adding prcommit as well as change to the lambda layer module
    
    * adding prcommit as well as change to the lambda layer module
    
    * adding gitignore
    
    * updated the trigger event for layer
    
    * Added bedrock in to main function
    
    * Implmented bedrock
    
    * removing not needed files
    
    * update event bridge
    
    * Fixing python
    
    * fixing python code
    
    * updating step function
    
    * updated terrform and code to fix issues
    
    * fixing lambda
    
    * fixing lambdap2
    
    * Update code

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -6,5 +6,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.7.0"
+    }
   }
 }
