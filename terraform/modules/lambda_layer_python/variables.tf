fb439c76daa2ad935eeba1cd02194b2c101415a8
forestnd <nig@ghostfacts.co.uk>
2025-03-20 19:58:39 +0000

    Fixing pre-commit and updating local.tf

--- terraform/modules/lambda_layer_python/variables.tf
+++ terraform/modules/lambda_layer_python/variables.tf
@@ -1,11 +1,14 @@
 variable "layer_name" {
+  type        = string
   description = "environment the code being deplyed"
 }
 
 variable "runtime" {
+  type        = string
   description = "environment the code being deplyed"
 }
 
 variable "modules" {
+  type        = list(string)
   description = "environment the code being deplyed"
 }
