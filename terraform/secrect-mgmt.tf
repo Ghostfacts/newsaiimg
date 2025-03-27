9242e3b6d93e6f158b37d87934eec25b14584b11
forstops <97102660+forstops@users.noreply.github.com>
2025-03-27 12:25:06 +0000

    undoing github secrect mgmt

--- terraform/secrect-mgmt.tf
+++ terraform/secrect-mgmt.tf
@@ -1,7 +1,12 @@
-##list of all secrects need (but requries manual entry for now)
+##list of all secrects need (but requries manual entry)
 resource "aws_secretsmanager_secret" "newsapi" {
   # checkov:skip=CKV2_AWS_57
   # checkov:skip=CKV_AWS_149
   name = "newsaiimg-${local.environment_map[var.environment]}-ssm-newsapi"
   tags = local.tags
 }
+
+# resource "aws_secretsmanager_secret_version" "newsapi" {
+#   secret_id     = aws_secretsmanager_secret.newsapi.id
+#   secret_string = data.github_actions_secrets.newsapi.secrets
+# }
