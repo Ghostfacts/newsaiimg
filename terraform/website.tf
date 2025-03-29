4a70349b1d534172387ae0e10c7af3d1ae31f8d0
forestnd <nig@ghostfacts.co.uk>
2025-03-29 20:38:59 +0000

    Adding checkov skips for website

--- terraform/website.tf
+++ terraform/website.tf
@@ -6,6 +6,7 @@ resource "aws_s3_bucket" "website_bucket" {
   # checkov:skip=CKV_AWS_21
   # checkov:skip=CKV2_AWS_61
   # checkov:skip=CKV_AWS_145
+  # checkov:skip=CKV_AWS_20
   bucket = "newsaiimg-${local.environment_map[var.environment]}-s3-website"
   acl    = "public-read"
 
