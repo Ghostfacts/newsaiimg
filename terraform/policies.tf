d5c7fbbbeda35ba3889a0a009ee5852e4bf55079
forestnd <nig@ghostfacts.co.uk>
2025-04-01 09:18:22 +0100

    updated sns and sqs

--- terraform/policies.tf
+++ terraform/policies.tf
@@ -3,7 +3,8 @@ data "aws_iam_policy_document" "lambda_policy" {
     sid    = "SNSqueue"
     effect = "Allow"
     actions = [
-      "SNS:SendMessage"
+      "SNS:SendMessage",
+      "SQS:SendMessage"
     ]
     resources = [aws_sqs_queue.dlq.arn]
   }
