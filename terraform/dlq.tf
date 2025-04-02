ff651612a971d4a250669d980ca99ac105b18f7b
forestnd <nig@ghostfacts.co.uk>
2025-04-03 00:08:45 +0100

    disable terrascan checks

--- terraform/dlq.tf
+++ terraform/dlq.tf
@@ -3,11 +3,23 @@ resource "aws_sns_topic" "sns_topic" {
   # checkov:skip=CKV_AWS_26
   # checkov:skip=CKV_AWS_27
   name = "newsaiimg-${local.environment_map[var.environment]}-sns-topic"
+  tags = merge(
+    local.tags,
+    {
+      Name = "newsaiimg-${local.environment_map[var.environment]}-sns-topic"
+    }
+  )
 }
 resource "aws_sqs_queue" "dlq" {
   # checkov:skip=CKV_AWS_26
   # checkov:skip=CKV_AWS_27
   name = "newsaiimg-${local.environment_map[var.environment]}-sqs-dlq"
+  tags = merge(
+    local.tags,
+    {
+      Name = "newsaiimg-${local.environment_map[var.environment]}-sns-topic"
+    }
+  )
 }
 
 resource "aws_sns_topic_subscription" "sns_topic_subscription" {
