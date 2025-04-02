27080fba500dbb2e3294d98fc3636f1ca2f54da2
forestnd <nig@ghostfacts.co.uk>
2025-04-02 22:43:57 +0100

    uncommented aws_s3_bucket_policy

--- terraform/website.tf
+++ terraform/website.tf
@@ -37,21 +37,21 @@ resource "aws_s3_bucket_public_access_block" "website_bucket" {
 }
 
 
-# # S3 Bucket Policy to Allow Public Read Access
-# resource "aws_s3_bucket_policy" "website_bucket_policy" {
-#   bucket = aws_s3_bucket.website_bucket.id
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect    = "Allow"
-#         Principal = "*"
-#         Action    = "s3:GetObject"
-#         Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
-#       }
-#     ]
-#   })
-# }
+# S3 Bucket Policy to Allow Public Read Access
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
 
 # CloudFront Distribution
 resource "aws_cloudfront_distribution" "cdn" {
