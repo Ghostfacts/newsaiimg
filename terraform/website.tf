7ec7ba4b9640ac3a7477e28e51605c667137aa09
forestnd <nig@ghostfacts.co.uk>
2025-04-04 00:04:57 +0100

    Fixing site inif lockdown s3 bucket

--- terraform/website.tf
+++ terraform/website.tf
@@ -30,23 +30,29 @@ resource "aws_s3_bucket_public_access_block" "website" {
   # checkov:skip=CKV_AWS_55
   # checkov:skip=CKV_AWS_53
   bucket                  = aws_s3_bucket.website.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "website_policy" {
-  # checkov:skip=CKV_AWS_70 #temp
   bucket = aws_s3_bucket.website.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.website.arn}/*"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn
+          }
+        }
       }
     ]
   })
