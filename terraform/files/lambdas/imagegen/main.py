2487ef3bf59b9fcb9ea0b93b8afcfe0880bd1ae2
forestnd <nig@ghostfacts.co.uk>
2025-04-08 21:30:48 +0100

    updated imagegen code

--- terraform/files/lambdas/imagegen/main.py
+++ terraform/files/lambdas/imagegen/main.py
@@ -15,16 +15,25 @@ logging.basicConfig(
     handlers=[logging.StreamHandler()],
 )
 
+s3_client = boto3.client("s3")
 
-def upload_to_s3(file_path, bucket_name, s3_key):
-    """Upload a file to an S3 bucket"""
-    s3_client = boto3.client("s3")
+
+def s3_write_file(bucketname, key, data):
+    """Write s3 file"""
     try:
-        s3_client.upload_file(file_path, bucket_name, s3_key)
-        logging.info("Uploading %s to s3://%s/%s", file_path, bucket_name, s3_key)
-    except Exception as e:
-        logging.error("Failed to upload %s to S3: %s", file_path, e)
-        raise
+        s3_client.put_object(Bucket=bucketname, Key=key, Body=data)
+    except Exception as e:  # pylint: disable=W0718
+        logging.error("S3 Write error: %s", str(e))
+
+
+def s3_read_file(bucketname, key):
+    """Read an se file"""
+    try:
+        response = s3_client.get_object(Bucket=bucketname, Key=key)
+        return str(response["Body"].read().decode("utf-8"))
+    except Exception as e:  # pylint: disable=W0718
+        logging.error("S3 Read error: %s", str(e))
+        return None
 
 
 # main
@@ -38,39 +47,49 @@ def lambda_handler(event, context):  # pylint: disable=W0613
             WithDecryption=True,
         )["Parameter"]["Value"]
     )
+    log_id = getattr(context, "log_stream_name", None)
+    newsai = genai.Genai(region=ssm_data["region"])
+    json_data = json.loads(
+        s3_read_file(
+            bucketname=ssm_data["ais3bucket"],
+            key=f"aiimg/{event.get('event_id')}/main.json",
+        )
+    )
+
     temp_dir = (
         tempfile.TemporaryDirectory()  # pylint: disable=R1732 #dont want to use an with
     )
-    log_id = getattr(context, "log_stream_name", None)
-    newsai = genai.Genai(region=ssm_data["region"])
-    # Create a temporary folder in /tmp/
     promt = newsai.generate_image(
-        story=event["story"], imagepath=os.path.join(temp_dir.name)
+        story=json_data["picked_article"], imagepath=os.path.join(temp_dir.name)
     )
-    # Write a JSON file to the temporary folder
-    json_data = {
-        "event_id": event.get("event_id"),
-        "ai_data": {
-            "prompt": promt.get("prompt"),
-            "model_id": promt.get("model_id"),
-        },
+    # Read the generated image from promt['imgpath']
+    try:
+        with open(promt["imgpath"], "rb") as image_file:
+            image_data = image_file.read()
+        # Write the image data to the S3 bucket
+        s3_write_file(
+            bucketname=ssm_data["ais3bucket"],
+            key=f"aiimg/{event.get('event_id')}/main.png",
+            data=image_data,
+        )
+    except Exception as e:  # pylint: disable=W0718
+        logging.error("Error reading or writing image: %s", str(e))
+
+    json_data["genimage"] = {
+        "prompt": promt.get("prompt"),
+        "model_id": promt.get("model_id"),
+        "imagepath": f"s3://{ssm_data["ais3bucket"]}/aiimg/{event.get('event_id')}/main.png",
     }
-    json_file_path = os.path.join(temp_dir.name, "gen_image_data.json")
-    with open(json_file_path, "w", encoding="utf-8") as json_file:
-        json.dump(json_data, json_file)
-    bucket_name = ssm_data["ais3bucket"]
-    for file_name in os.listdir(temp_dir.name):
-        file_path = os.path.join(temp_dir.name, file_name)
-        s3_key = f"aiimg/{event.get('event_id')}/{file_name}"
-        upload_to_s3(file_path, bucket_name, s3_key)
+    s3_write_file(
+        bucketname=ssm_data["ais3bucket"],
+        key=f"aiimg/{event.get("event_id")}/main.json",
+        data=json.dumps(json_data, indent=2).encode("utf-8"),
+    )
     temp_dir.cleanup()
     return {
         "statusCode": promt.get("status_code"),
-        "image_path": f"s3://{ssm_data['ais3bucket']}/aiimg/{event.get('event_id')}/newsimage.png",
-        "ai_data": {
-            "prompt": promt.get("prompt"),
-            "model_id": promt.get("model_id"),
-        },
+        "image_path": f"s3://{ssm_data["ais3bucket"]}/aiimg/{event.get('event_id')}/main.png",
+        "ai_data": json_data["genimage"],
         "error": promt.get("error"),
         "log_id": log_id,
     }
