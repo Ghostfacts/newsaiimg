c4a9de55ef5f73e4381a4f59083ee17c1771cbdf
forestnd <nig@ghostfacts.co.uk>
2025-05-04 08:50:57 +0100

    fix:(UPdating lambdas and ssm): UPdated the lambda to suport to changes in ssm

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -32,8 +32,7 @@ module "news_api_function" {
   function_handler = "main.lambda_handler"
   timeout          = 830
   environment_variables = {
-    secrect_name = aws_secretsmanager_secret.newsapi.name
-    region_name  = data.aws_region.current.name
+    SSM_PARAMETER_NAME = aws_ssm_parameter.json_parameter.name
   }
   dlq_arn       = aws_sqs_queue.dlq.arn
   attach_layers = [module.news_api_layer.layer.arn]
@@ -49,16 +48,18 @@ module "img_gen_function" {
   # checkov:skip=CKV_AWS_116
   # checkov:skip=CKV_AWS_117
   # checkov:skip=CKV_AWS_173
-  source                = "./modules/lambda_function"
-  nameprefex            = "newsaiimg-${local.environment_map[var.environment]}-imggen"
-  runtime               = "python3.10"
-  source_path           = "files/lambdas/createimg/"
-  function_handler      = "main.lambda_handler"
-  timeout               = 830
-  environment_variables = {}
-  dlq_arn               = aws_sqs_queue.dlq.arn
-  attach_layers         = [module.image_gen_layer.layer.arn]
-  policy                = data.aws_iam_policy_document.lambda_policy.json
+  source           = "./modules/lambda_function"
+  nameprefex       = "newsaiimg-${local.environment_map[var.environment]}-imggen"
+  runtime          = "python3.10"
+  source_path      = "files/lambdas/createimg/"
+  function_handler = "main.lambda_handler"
+  timeout          = 830
+  environment_variables = {
+    SSM_PARAMETER_NAME = aws_ssm_parameter.json_parameter.name
+  }
+  dlq_arn       = aws_sqs_queue.dlq.arn
+  attach_layers = [module.image_gen_layer.layer.arn]
+  policy        = data.aws_iam_policy_document.lambda_policy.json
   tags = merge(
     local.tags
   )
@@ -70,16 +71,18 @@ module "pagedeploy_function" {
   # checkov:skip=CKV_AWS_116
   # checkov:skip=CKV_AWS_117
   # checkov:skip=CKV_AWS_173
-  source                = "./modules/lambda_function"
-  nameprefex            = "newsaiimg-${local.environment_map[var.environment]}-webpagedesign"
-  runtime               = "python3.10"
-  source_path           = "files/lambdas/createwebpost/"
-  function_handler      = "main.lambda_handler"
-  timeout               = 830
-  environment_variables = {}
-  dlq_arn               = aws_sqs_queue.dlq.arn
-  attach_layers         = [module.image_gen_layer.layer.arn]
-  policy                = data.aws_iam_policy_document.lambda_policy.json
+  source           = "./modules/lambda_function"
+  nameprefex       = "newsaiimg-${local.environment_map[var.environment]}-webpagedesign"
+  runtime          = "python3.10"
+  source_path      = "files/lambdas/createwebpost/"
+  function_handler = "main.lambda_handler"
+  timeout          = 830
+  environment_variables = {
+    SSM_PARAMETER_NAME = aws_ssm_parameter.json_parameter.name
+  }
+  dlq_arn       = aws_sqs_queue.dlq.arn
+  attach_layers = [module.image_gen_layer.layer.arn]
+  policy        = data.aws_iam_policy_document.lambda_policy.json
   tags = merge(
     local.tags
   )
